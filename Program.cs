50187adf2372ca201ca0a994d654acdb7b8d2b4d
hammarhank <axtr@live.com>
2023-02-24 09:18:01 +0100

    publik metod Print i Grundämne som skriver ut grundämnet

--- Program.cs
+++ Program.cs
@@ -1,10 +1,43 @@
-﻿namespace IDP_assg_3
+﻿using System;
+
+namespace IDP_assg_3
 {
     internal class Program
     {
+        public class Grundämne
+        {
+            public string Namn;
+            public int Z;
+            public string Typ;
+            public double Smältpunkt;
+            public double Kokpunkt;
+            public Grundämne(string namn, int z, string typ, double smältpunkt, double kokpunkt)
+            {
+                Namn = namn;
+                Z = z;
+                Typ = typ;
+                Smältpunkt = smältpunkt;
+                Kokpunkt = kokpunkt;
+            }
+            public void Print()
+            {
+                
+                    Console.ForegroundColor = ConsoleColor.Cyan;
+                    Console.WriteLine($"Grundämne: {Namn.ToUpper()}\n Typ: {Typ}\n Smältpunkt: {Smältpunkt}K\n Kokpunkt: {Kokpunkt}K ");
+                    Console.WriteLine();
+
+                
+            }
+        }
         static void Main(string[] args)
         {
-            
+            Grundämne syre = new Grundämne("Syre", 8, "ickemetall", 54.36, 90.188);
+            Grundämne järn = new Grundämne("Järn", 26, "metall", 1811, 3134);
+            Grundämne guld = new Grundämne("Guld", 79, "metall", 1337.33, 3243);
+
+            syre.Print();
+            järn.Print();
+            guld.Print();
         }
     }
 }
